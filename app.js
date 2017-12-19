a0acc0185ca3b415d2ee11cb80a5568c4469294b
Katy Robinson <katyorob@gmail.com>
2017-12-18 21:11:34 -0800

    images consistant size

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function Product (name, filepath) {
 
 //creating new instances
 
-new Product ('bag', 'img/bag.jpg');
+new Product ('bag', 'img/bag.png');
 new Product ('banana', 'img/banana.jpg');
 new Product ('bathroom', 'img/bathroom.jpg');
 new Product ('boots', 'img/boots.jpg');
@@ -27,7 +27,7 @@ new Product ('cthulhu', 'img/cthulhu.jpg');
 new Product ('dog duck', 'img/dog-duck.jpg');
 new Product ('dragon', 'img/dragon.jpg');
 new Product ('pen', 'img/pen.jpg');
-new Product ('pet sweep', 'img/pet-sweep');
+new Product ('pet sweep', 'img/pet-sweep.jpg');
 new Product ('scizzors', 'img/scizzors.jpg');
 new Product ('shark', 'img/shark.jpg');
 new Product ('sweep', 'img/sweep.png');
